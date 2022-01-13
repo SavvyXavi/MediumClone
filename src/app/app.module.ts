3174974ccf68266257ecd4d2c6b5d54b7df18d7b
Xavi-Dev <xjohndev@outlook.com>
2022-01-13 17:33:09 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/components/article/article.component';
+import { AuthModule } from './auth/auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RegisterComponent } from '../components/register/register.component';
+
+const routes = [
+ {
+   path: 'register',
+   component: RegisterComponent
+ } 
+]
+
+@NgModule({
+  declarations: [
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule { }
