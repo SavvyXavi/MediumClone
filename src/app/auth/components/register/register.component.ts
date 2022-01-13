de3ac5634956f0a72b69305361ec1f9f6b87f86f
Xavi-Dev <xjohndev@outlook.com>
2022-01-13 16:47:01 -0500

    Med Clone init/ registration init

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+ }
+
+ onSubmit(): void {
+   console.log('submit', this.form.value, this.form.valid)
+ }
+}
