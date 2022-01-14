f773bcd69e5b5b6e8bf061929c5219633b419a8e
Xavi-Dev <xjohndev@outlook.com>
2022-01-14 11:18:12 -0500

    install and setup for NgRx store/Action types and action file created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
+import { AuthModule } from './auth/auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/components/article/article.component';
-import { AuthModule } from './auth/auth/auth.module';
+import { StoreDevtools, StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { AuthModule } from './auth/auth/auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, //retains last 25 stores
+      logOnly: environment.production //restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from 'src/app/store/actions';
+
 
 @Component({
   selector: 'app-register',
@@ -9,7 +12,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private store: Store) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -25,5 +28,6 @@ export class RegisterComponent implements OnInit {
 
  onSubmit(): void {
    console.log('submit', this.form.value, this.form.valid)
+   this.store.dispatch(registerAction(this.form.value))
  }
 }
